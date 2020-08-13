1fd6fe97f7b66cf427b1ed84d0ccfc756b6f2f67
koike <koike@logic-biz.co.jp>
2020-08-13 19:16:18 +0900

    テストコメント

--- HookAppUnitTest/UnitTest1.cs
+++ HookAppUnitTest/UnitTest1.cs
@@ -3,8 +3,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace HookAppUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ModelTests
     {
+        /// <summary>
+        /// バージョンプロパティ情報が正しく取得できることを確認します。
+        /// </summary>
         [TestMethod]
         public void VersionInfoPropertyCheck()
         {
