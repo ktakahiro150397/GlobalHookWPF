1b0b9037fc7b469ba0d4d6b4d5068a6c1656587b
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2021-06-15 13:56:59 +0900

    バリデーション部分実装完了

--- HookAppUnitTest/Models/SettingFileValidatorTest.cs
+++ HookAppUnitTest/Models/SettingFileValidatorTest.cs
@@ -13,11 +13,26 @@ namespace HookAppUnitTest.Models
     public class SettingFileValidatorTest : TestBase
     {
 
+        /// <summary>
+        /// すべてのキーが含まれているかどうかのチェック
+        /// </summary>
         [TestMethod]
-        public void AllKeyContainsTest()
+        public void NoExistKeyCodeList_Correct()
         {
 
             var testsettings = new List<LoadedPicSettings>() {
+                new LoadedPicSettings()
+                {
+                    KeyName = "Escape",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F1",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
                 new LoadedPicSettings()
                 {
                     KeyName = "F2",
@@ -28,21 +43,114 @@ namespace HookAppUnitTest.Models
 
             //実際の処理
             SettingFileValidator actualValidator = new SettingFileValidator(testsettings);
-            SettingFileValidatorResult actualResult = actualValidator.GetKeyContainsValidationResult();
+            SettingFileValidatorResult actualResult = actualValidator.GetValidationResult();
 
-            //期待する出力
-            //対応ディクショナリとの差集合リストを取得する
-            var expectedList = KeyboardUtilConstants.keyNameKeyCodeDictionary.Values.Except(testsettings.Select((item) => KeyboardUtilConstants.keyNameKeyCodeDictionary[item.KeyName])).ToList();
-            SettingFileValidatorResult expectedResult = new SettingFileValidatorResult(expectedList);
+            //期待する出力 : 存在しないキーなし
+            SettingFileValidatorResult expectedResult = new SettingFileValidatorResult();
 
             //比較
-            expectedResult.AssertIs(actualResult);
+            actualResult.NoExistKeyCodeList.AssertIs(expectedResult.NoExistKeyCodeList);
+
+        }
+
+        /// <summary>
+        /// すべてのキーが含まれないチェック
+        /// </summary>
+        [TestMethod]
+        public void NoExistKeyCodeList_NoExists()
+        {
+
+            var testsettings = new List<LoadedPicSettings>() {
+                new LoadedPicSettings()
+                {
+                    KeyName = "Escape",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F2",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                }
+            };
+
+            //実際の処理
+            SettingFileValidator actualValidator = new SettingFileValidator(testsettings);
+            SettingFileValidatorResult actualResult = actualValidator.GetValidationResult();
+
+            //期待する出力
+            //F1が存在しない
+            SettingFileValidatorResult expectedResult = new SettingFileValidatorResult();
+            expectedResult.NoExistKeyCodeList.AddRange(new List<KeyboardUtilConstants.VirtualKeyCode>() { KeyboardUtilConstants.VirtualKeyCode.F1 });
+
+            //比較
+            actualResult.NoExistKeyCodeList.AssertIs(expectedResult.NoExistKeyCodeList);
 
         }
 
 
+        /// <summary>
+        /// 設定ファイル中の重複キーチェック
+        /// </summary>
+        [TestMethod]
+        public void NoExistKeyCodeList_DuplicateKeyList()
+        {
+            var testsettings = new List<LoadedPicSettings>() {
+                new LoadedPicSettings()
+                {
+                    KeyName = "Escape",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F1",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F1",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F2",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F2",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                },
+                new LoadedPicSettings()
+                {
+                    KeyName = "F2",
+                    KeyPicName = "NormalKey.png",
+                    keyPosInfo = new LoadedPicSettings.KeyPos(99D, 999D),
+                }
+            };
 
+            //実際の処理
+            SettingFileValidator actualValidator = new SettingFileValidator(testsettings);
+            SettingFileValidatorResult actualResult = actualValidator.GetValidationResult();
 
+            //期待する出力
+            //F1が存在しない
+            SettingFileValidatorResult expectedResult = new SettingFileValidatorResult();
+            expectedResult.DuplicateKeyList.AddRange(new List<SettingFileValidatorResult.SettingFileValidatorDuplicateResult>() { 
+                new SettingFileValidatorResult.SettingFileValidatorDuplicateResult(KeyboardUtilConstants.VirtualKeyCode.F1,2),
+                new SettingFileValidatorResult.SettingFileValidatorDuplicateResult(KeyboardUtilConstants.VirtualKeyCode.F2,3)
+            });
+
+            //比較
+            actualResult.NoExistKeyCodeList.AssertIs(expectedResult.NoExistKeyCodeList);
+
+        }
 
     }
 }
